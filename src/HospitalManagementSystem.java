b58b55e7784ae7c868f06df1927e0d3fdcbdc7b6
M Bilal <bilal.12542@gmail>
2020-06-09 15:59:43 +0500

    first commit

---
+++ src/HospitalManagementSystem.java
@@ -0,0 +1,13 @@
+
+public interface HospitalManagementSystem {
+	public void Hospital();
+	public void Person();
+	public void TechnicalStaff();
+	public void Receptionist();
+	public void Nurse();
+	public void Doctor();
+	public void Patient();
+	public void Ward();
+	public void Record();
+	public void Prescription();
+}
---
+++ src/Person.java
@@ -0,0 +1,5 @@
+
+public class Person {
+	private String full_name, gender, address;
+	private int id, phone;
+}
