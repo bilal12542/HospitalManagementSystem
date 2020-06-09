3472e295252c4a60cbd6392964d89accf0a41abd
M Bilal <bilal.12542@gmail>
2020-06-09 19:01:56 +0500

    second commit

---
+++ src/Hospital.java
@@ -0,0 +1,10 @@
+
+public class Hospital {
+	private int id;
+	private String address, hospital_name;
+	Hospital(int id, String address, String hospital_name){
+		this.id = id;
+		this.hospital_name = hospital_name;
+		this.address = address;
+	}
+}
--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,11 @@
 public class Person {
 	private String full_name, gender, address;
 	private int id, phone;
+	Person (int id, String full_name, String gender, String address, int phone){
+		this.id = id;
+		this.full_name = full_name;
+		this.address = address;
+		this.gender = gender;
+		this.phone = phone;
+	}
 }
