2f822a75f9c0372da8cfb4bfdbb24e9bd56db24d
SaroshJaved <66783344+SaroshJaved@users.noreply.github.com>
2020-06-12 00:07:35 +0500

    Update Doctor.java

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,4 +1,12 @@
 
 public class Doctor {
-	
+	private String specialization, department;
+	private int id;
+	Doctor (int id, String specialization, String department){
+		this.id = id;
+		this.specialization= specialization;
+		this.department = department;
+	}
 }
+
+
