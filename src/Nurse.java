547f1fd1872d3d714147d51bd8935b48a34fc32b
M Bilal <bilal.12542@gmail>
2020-06-16 18:55:10 +0500

    2nd draft

--- src/Nurse.java
+++ src/Nurse.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 
 public class Nurse {
 	private String department;
-	List<Integer> Ward = new ArrayList<>();
+	ArrayList<Integer> Ward = new ArrayList<>();
 	Nurse (String department){
 		this.department = department;	
 		this.Ward = Ward;
