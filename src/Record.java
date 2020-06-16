c1e82b8a8fa8100e0a86a12a84ee1a23c7e06397
M Bilal <bilal.12542@gmail>
2020-06-17 01:00:05 +0500

    2nd draft

--- src/Record.java
+++ src/Record.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
 
 public class Record {
-
+	private String patient_id, date, treatment, sickness;
+	ArrayList<String> medicines = new ArrayList<String>();
+	public Record() {
+		
+	}
+	
 }
