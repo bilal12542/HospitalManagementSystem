0f9fe179b9e9122a71edeebed98cff14784de446
SaroshJaved <66783344+SaroshJaved@users.noreply.github.com>
2020-06-19 02:29:32 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,17 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		FileHandling file = new FileHandling("hospital1.xlsx");
-//		Doctor doc = new Doctor(file);
-//		doc.newDoctor(file);
+		Patient Pat = new Patient(file);
+		Pat.newPatient(file);
+		Pat.viewAllPat();
+		
 //		Staff staff = new Staff(file);
-		Medicine Meds = new Medicine(file);
-		Meds.newMed(file);
+//		Medicine Meds = new Medicine(file);
+		
+//		Meds.newMed(file);
 //		staff.newStaff(file);
-		Meds.viewAllMeds();
+		
+//		Meds.viewAllMeds();
 //		staff.newStaff(file);
 //		staff.viewAllStaff();
 	}
