77b24d08cacf60c45ca791295f1cc3568ce0c586
M Bilal <bilal.12542@gmail>
2020-06-19 22:26:58 +0500

    2nd draft

--- src/Main.java
+++ src/Main.java
@@ -9,40 +9,40 @@ public class Main {
 		Nurse nur = new Nurse(file);
 		Staff staff = new Staff(file);
 		Labs lab = new Labs(file);
-		while(true) {
-			System.out.println("For Doctor press '1'");
-			System.out.println("For Nurse press '2'");
-			System.out.println("For Staff press '3'");
-			System.out.println("For Labs press '4'");
-			System.out.println("View All Tables press '5'");
-			switch(sc.nextInt()){
-			case 0:
-				break;
-			case 1:
-				while(true) {
-					System.out.println("View Press '1'");
-					System.out.println("Add Press '2'");
-					System.out.println("Main Menu Press '3'");
-					switch(sc.nextInt()) {
-					case 2:
-						doc.viewAllDoc();
-					case 1:
-						doc.newDoctor(file);
-						doc.viewAllDoc();
-					case 3:
-						break;
-					default:
-						System.out.println("Invalid Option");
-					}
-				}
-			case 5:
-				doc.viewAllDoc();
-				nur.viewAllNurse();
-				staff.viewAllStaff();
-				lab.viewAllLabs();
-			default:
-				System.out.println("Invalid Option");
-			}
-		}
+//		while(true) {
+//			System.out.println("For Doctor press '1'");
+//			System.out.println("For Nurse press '2'");
+//			System.out.println("For Staff press '3'");
+//			System.out.println("For Labs press '4'");
+//			System.out.println("View All Tables press '5'");
+//			switch(sc.nextInt()){
+//			case 0:
+//				break;
+//			case 1:
+//				while(true) {
+//					System.out.println("View Press '1'");
+//					System.out.println("Add Press '2'");
+//					System.out.println("Main Menu Press '3'");
+//					switch(sc.nextInt()) {
+//					case 2:
+//						doc.viewAllDoc();
+//					case 1:
+//						doc.newDoctor(file);
+//						doc.viewAllDoc();
+//					case 3:
+//						break;
+//					default:
+//						System.out.println("Invalid Option");
+//					}
+//				}
+//			case 5:
+//				doc.viewAllDoc();
+//				nur.viewAllNurse();
+//				staff.viewAllStaff();
+//				lab.viewAllLabs();
+//			default:
+//				System.out.println("Invalid Option");
+//			}
+//		}
 	}
 }
