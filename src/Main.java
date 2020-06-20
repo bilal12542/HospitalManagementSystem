a9afe0573a0d5a100aafcbaff32982aba53d9aae
SaroshJaved <66783344+SaroshJaved@users.noreply.github.com>
2020-06-20 15:16:02 +0500

    abcc

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 		Medicine Med = new Medicine(file);
 		Labs lab = new Labs(file);
 		Record rec = new Record(file);
-		rec.newRecord(file);
+		staff.newStaff(file);
 //		while(true) {
 //			System.out.println("For Doctor press '1'");
 //			System.out.println("For Nurse press '2'");
